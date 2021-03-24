30194cfb20d4007bce3497ff804a764d8e16be71
ZhiXiang <55120640+xx8897@users.noreply.github.com>
2021-03-24 16:33:59 +0800

    Add files via upload

--- quiz1.js
+++ quiz1.js
@@ -1,1 +1,8 @@
-quiz1.js
+function change() {
+    var header = document.getElementById("hid123");
+    header.innerHTML = "CSIE@CGU";
+   
+    var para = document.getElementById("pid123");
+    para.innerHTML = "怎麼那麼棒！！.";
+   
+    }
