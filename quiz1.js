a202b5943ecb545a76bf04b43cfdb1156d7098a0
ZhiXiang <55120640+xx8897@users.noreply.github.com>
2021-05-11 17:28:48 +0800

    修正

--- quiz1.js
+++ quiz1.js
@@ -1,8 +1,21 @@
 function change() {
-    var header = document.getElementById("hid123");
-    header.innerHTML = "CSIE@CGU";
-   
-    var para = document.getElementById("pid123");
-    para.innerHTML = "怎麼那麼棒！！.";
-   
-    }
+    /*
+    document.getElementById('cgu').innerHTML = "CSIE@CGU"; 
+    document.getElementById('good').innerHTML = "怎麼那麼棒！！.";     
+    */
+   $("#cgu").html("CSIE@CGU");
+   $("#good").html("怎麼那麼棒！！.");
+}
+function new_button(){
+    /*
+    var btn = document.createElement("BUTTON"); 
+    btn.innerHTML = "Change this document"; 
+    document.body.appendChild(btn); 
+    btn.addEventListener("click",change);
+    */
+    var change_btn = "<button id='change'>Change this document</button>";
+    $("body").append(change_btn);
+    $("#change").click(change);
+}
+
+$("#btn").click(new_button);
--- quiz2.js
+++ quiz2.js
@@ -1,13 +1,13 @@
-var tags = "cat";
-var dataUrl = "https://www.flickr.com/services/feeds/photos_public.gne?tags=cat&tagmode=any&format=json&per_page=400&jsoncallback=?";
-var data = $.getJSON(dataUrl);
-data.done( function( msg ) {
- $.each(msg.items, function(i,item){
-   $("#contain").html();
-   $("#contain").append($("<img/>").attr("src", item.media.m));
- });
-});
-data.fail( function( msg ) {
- console.log(msg);
- $("#contain").html("fail getting data");
+
+var tags = "吉娃娃";
+var dataUrl = "https://api.flickr.com/services/feeds/photos_public.gne?tags=" + tags+"&tagmode=any&format=json&per_page=400&jsoncallback=?"; 
+var data = $.getJSON(dataUrl); 
+data.done( function(msg) {
+    console.log(msg.items);
+    $.each(msg.items,function(key,val){
+        $('#contain').append($("<img/>").attr("src",val.media.m)); 
+    });
+}); 
+data.fail( function(msg) { 
+    $('#contain').html('Failed');
 });
